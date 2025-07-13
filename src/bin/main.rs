2644f1a045b0392be8d0cd4bee56d3b6624c44ae
bee <bernhard.spindler.75@gmail.com>
2025-07-13 17:56:34 +0200

    GEHT!

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,12 +5,22 @@
     reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
     holding buffers for the duration of a data transfer."
 )]
+#![feature(type_alias_impl_trait)]
+
+//extern crate alloc;
 
 use embassy_executor::Spawner;
+use embassy_net::{DhcpConfig, Runner, StackResources};
 use embassy_time::{Duration, Timer};
 use esp_hal::clock::CpuClock;
+use esp_hal::peripherals::WIFI;
 use esp_hal::timer::systimer::SystemTimer;
+use esp_hal::timer::timg::TimerGroup;
 use esp_println::println;
+use esp_wifi::wifi::{ClientConfiguration, Configuration, WifiController, WifiDevice, WifiEvent, WifiState};
+use esp_wifi::EspWifiController;
+use static_cell::StaticCell;
+
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -21,19 +31,68 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
+macro_rules! singleton {
+    ($val:expr) => {{
+        type T = impl Sized;
+        static STATIC_CELL: StaticCell<T> = StaticCell::new();
+        let (x,) = STATIC_CELL.init(($val,));
+        x
+    }};
+}
+
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
     // generator version: 0.4.0
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
-
     let timer0 = SystemTimer::new(peripherals.SYSTIMER);
     esp_hal_embassy::init(timer0.alarm0);
     esp_println::logger::init_logger(log::LevelFilter::Info);
+    esp_alloc::heap_allocator!(size: 64 * 1024);
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
+    let mut rng = esp_hal::rng::Rng::new(peripherals.RNG);
+
+    let timer1 = TimerGroup::new(peripherals.TIMG0);
+
+    static WIFI_CONTROLLER_CELL : StaticCell<EspWifiController> = StaticCell::new();
+    let esp_wifi_ctrl = WIFI_CONTROLLER_CELL.init(
+        esp_wifi::init(
+            timer1.timer0,
+            rng.clone(),
+            peripherals.RADIO_CLK,
+        )
+        .unwrap()
+    );
+    /*
+    let esp_wifi_ctrl = singleton!(
+        esp_wifi::init(
+            timer1.timer0,
+            rng.clone(),
+            peripherals.RADIO_CLK,
+        )
+        .unwrap()
+    );*/
+
+
+    let (controller, interfaces) = esp_wifi::wifi::new(esp_wifi_ctrl, peripherals.WIFI).unwrap();
+    let wifi_interface = interfaces.sta;
+
+    let net_seed = rng.random() as u64 | ((rng.random() as u64) << 32);
+    let tls_seed = rng.random() as u64 | ((rng.random() as u64) << 32);
+
+    let net_config = embassy_net::Config::dhcpv4(DhcpConfig::default());
+    
+    // Init network stack
+    let (network_stack , runner) = embassy_net::new(
+        wifi_interface,
+        net_config,
+        singleton!(StackResources::<3>::new()),
+        net_seed,
+    );
+
+    spawner.spawn(connection_task(controller)).ok();
+    spawner.spawn(net_task(runner)).ok();
 
     loop {
         Timer::after(Duration::from_secs(1)).await;
@@ -42,3 +101,58 @@ async fn main(spawner: Spawner) {
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.1/examples/src/bin
 }
+/*
+#[embassy_executor::task]
+async fn net_task(stack: &'static Stack<WifiDevice<'static, WifiStaDevice>>) {
+    stack.run().await
+}
+*/
+#[embassy_executor::task]
+async fn net_task(mut runner : Runner<'static, WifiDevice<'static>> ) {
+    runner.run().await
+}
+
+
+#[embassy_executor::task]
+async fn connection_task(mut controller: WifiController<'static>) {
+    println!("start connection task");
+    println!("Device capabilities: {:?}", controller.capabilities());
+    loop {
+        match esp_wifi::wifi::wifi_state() {
+            WifiState::StaConnected => {
+                // wait until we're no longer connected
+                controller.wait_for_event(WifiEvent::StaDisconnected).await;
+                Timer::after(Duration::from_millis(5000)).await
+            }
+            _ => {}
+        }
+
+        const SSID: &str = "BabyStube";
+        const PASSWORD: &str = "PaulaNadja1977";
+
+        if !matches!(controller.is_started(), Ok(true)) {
+            let client_config = Configuration::Client(ClientConfiguration {
+                ssid: SSID.try_into().unwrap(),
+                password: PASSWORD.try_into().unwrap(),
+                ..Default::default()
+            });
+            controller.set_configuration(&client_config).unwrap();
+            println!("Starting wifi");
+            controller.start_async().await.unwrap();
+            println!("Wifi started!");
+        }
+        println!("About to connect...");
+
+        match controller.connect_async().await {
+            Ok(_) => println!("Wifi connected!"),
+            Err(e) => {
+                println!("Failed to connect to wifi: {e:?}");
+                Timer::after(Duration::from_millis(5000)).await
+            }
+        }
+    }
+}
+
+
+
+
